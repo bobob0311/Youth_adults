9d61405cb46d095ae8e9a84a8b1a353f6aea049e
bosung si <qweasdzxc0311@naver.com>
2025-01-21 19:26:54 +0900

    checkbutton location 클릭 시 변경 및 list button 크기 조절

--- components/button/CheckButtons.tsx
+++ components/button/CheckButtons.tsx
@@ -27,7 +27,6 @@ export default function CheckButton(props: PropsState) {
     const [selectedIndex, setSelectedIndex] = useState<number>(-1);
 
     // selectedIndex가 -1이 아니면 true를 위로 넘길 수 있게 로직설정
-    // 선택된게 다시 선택될 경우 지울 수 있게?
 
     const handleClick = (index: number) => {
         if (selectedIndex == index) {
@@ -43,30 +42,37 @@ export default function CheckButton(props: PropsState) {
     if (info.type === "label") {
         const { label, checkList } = info;
         
-        content = <div>
-            <div>{label}</div>
-            {checkList.map((item, idx) => (
-                <button
-                    key={idx}
-                    onClick={() => handleClick(idx)}
-                    className={`${styles.btn} ${idx === selectedIndex ? styles.selected : ""}`}
-                >
-                    {item}
-                </button>
-            ))}
-        </div>
+        content = 
+            <label className={styles.wrapper}>
+                {label}
+                <div className={styles.btnContainer}>
+                    {checkList.map((item, idx) => (
+                        <button
+                            key={idx}
+                            onClick={() => handleClick(idx)}
+                            className={`${styles.btn} ${idx === selectedIndex ? styles.selected : ""}`}
+                        >
+                            {item}
+                        </button>
+                    ))}
+                </div>
+            </label>
+        
     } else {
         const { checkList } = info;
 
         content = <div>
             {
                 checkList.map((item, idx) => (
-                    <button
+                    <button className={styles.imgBtn}
                         key={item.title}
                         onClick={() => handleClick(idx)}
                     >
-                        <h2>{item.title}</h2>
-                        <img src={item.url}></img>
+                        <img
+                            className={`${styles.locationImg} ${idx === selectedIndex ? styles.imgSelected :""}`}
+                            src={item.url}
+                            alt={item.title}
+                        />
                     </button>
                 ))
             }
