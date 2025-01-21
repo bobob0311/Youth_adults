e6f96ed03da4d6e3471c58b76743040ea9c1489e
bosung si <qweasdzxc0311@naver.com>
2025-01-21 19:27:31 +0900

    자잘한 수정

--- app/(info)/step1/page.tsx
+++ app/(info)/step1/page.tsx
@@ -2,21 +2,25 @@
 
 import CheckButton from "@/components/button/CheckButtons";
 import NavigationButton from "@/components/button/NavigationButton";
+import styles from "./LocationPage.module.scss";
 
 let DUMMY_INFO = [
-    { title: "강남/신논현", url: "zz" },
-    { title: "홍대/합정", url: "sdfasdf" },
-    { title: "이태원", url: "sdfasdf" },
-    { title: "건대입구", url: "sdfasdf" },
+    { title: "강남/신논현", url: "location1.png" },
+    { title: "홍대/합정", url: "location2.png" },
+    { title: "이태원", url: "location3.png" },
+    { title: "건대입구", url: "location4.png" },
 ]
 
 
-export default function Page() {
+export default function LocationPage() {
     return (
-        <div>
-            <h2>현재 위치를 선택해주세요</h2>
+        <>
+            <h2 className={styles.title}>현재 위치를 선택해주세요</h2>
+            <section className={styles.location}>
+            
             <CheckButton info= {{type: "location", checkList : DUMMY_INFO}}/>
             <NavigationButton title="다음으로" url="/step2"/>
-        </div>
+            </section>
+        </>
     )
 }
--- app/(info)/step2/page.tsx
+++ app/(info)/step2/page.tsx
@@ -2,15 +2,16 @@
 
 import CheckButton from "@/components/button/CheckButtons";
 import NavigationButton from "@/components/button/NavigationButton";
+import styles from "./selectionPage.module.scss"
 
-export default function Page() {
+export default function SelectionPage() {
     return (
-        <div>
-            <h2>그룹 정보를 선택해주세요</h2>
+        <>
+            <h2 className={styles.title}>그룹 정보를 선택해주세요</h2>
             <CheckButton info={{ type: "label", label: "인원 수", checkList: ['2명', '3명', '4명'] }} />
             <CheckButton info={{ type: "label", label: "그룹 성별", checkList: ['여성', '남성', '혼성'] }} />
             <CheckButton info= {{type: "label", label: "평균 연령",checkList : ['20대 초','20대 초','20대 초']}}/>
             <NavigationButton title="다음으로" url="/step3"/>
-        </div>
+        </>
     )
 }
