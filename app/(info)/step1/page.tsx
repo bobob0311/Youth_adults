cca4b0f09d7710d06d264ad68ab7f035d8ae1dc9
bosung si <qweasdzxc0311@naver.com>
2025-01-23 19:24:26 +0900

    포함된 checkBox 모두 선택시 활성화

--- app/(info)/step1/page.tsx
+++ app/(info)/step1/page.tsx
@@ -3,6 +3,7 @@
 import CheckButton from "@/components/button/CheckButtons";
 import NavigationButton from "@/components/button/NavigationButton";
 import styles from "./LocationPage.module.scss";
+import { useState } from "react";
 
 let DUMMY_INFO = [
     { title: "강남/신논현", url: "location1.png" },
@@ -13,13 +14,18 @@ let DUMMY_INFO = [
 
 
 export default function LocationPage() {
+    const [isBtnValid, setIsBtnValid] = useState<boolean>(false);
+    const handleValid = (chk:boolean) => {
+        setIsBtnValid(chk);
+    }
+
     return (
         <div className={styles.container}>
             <section className={styles.location}>
                 <h2 className={styles.title}>현재 위치를 선택해주세요</h2>
-                <CheckButton info= {{type: "location", checkList : DUMMY_INFO}}/>
+                <CheckButton info={{ type: "location", checkList: DUMMY_INFO }} onValid={(chk) => handleValid(chk) } />
             </section>
-            <NavigationButton title="다음으로" url="/step2"/>
+            <NavigationButton isValid={isBtnValid}  title="다음으로" url="/step2"/>
         </div>
     )
 }
