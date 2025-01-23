ec7914eb04c2314938735a59f6a6691f870c792b
bosung si <qweasdzxc0311@naver.com>
2025-01-23 18:18:05 +0900

    뒤로가기 버튼 추가 및 프로그레스바 추가

--- app/(info)/layout.tsx
+++ app/(info)/layout.tsx
@@ -1,13 +1,36 @@
+'use client'
+
+import { usePathname, useRouter } from "next/navigation";
 import styles from "./layout.module.scss";
+import ProgressBar from "@/components/ProgressBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const router = useRouter();
+    const path = usePathname();
+    
+    const isStep = (path:string) => {
+        if (path.substring(1, 5) === "step") {
+            
+            return  <ProgressBar range={path[5]}/>;
+        } else {
+            return null;
+        }
+    }
+
     return (
         <>
             <header className={styles.header}>
-                <button> 뒤로 가기 버튼</button>
+                <button
+                    className={styles.btn}
+                    onClick={() => router.back()}
+                    aria-label="뒤로 가기 버튼"
+                >
+                    <img src="/Back.svg" />
+                </button>
                 <h1 className={styles.title}><img src="/mainImg.png" alt="청춘 상회"/></h1>
             </header>
             <main>
+                {isStep(path)}
                 <div className={styles.content}>
                     {children}
                 </div>
