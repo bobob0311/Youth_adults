30a7b3720c21043386838b2e0c2e7ed60d1652e7
bosung si <qweasdzxc0311@naver.com>
2025-01-27 00:10:35 +0900

    imgslider 데이터 보내기

--- app/match/info/page.tsx
+++ app/match/info/page.tsx
@@ -3,15 +3,28 @@ import { useState } from "react"
 import styles from "./page.module.scss"
 import NavigationButton from "@/components/button/NavigationButton";
 import Link from "next/link";
+import ImgSlider from "./imgSlider";
+
+const IMGINFO = [
+    {
+        id:0,
+        title: "매칭그룹 소개",
+        imgSrc: "/location1.png",
+    },
+    {
+        id:1,
+        title: "매칭 프로세스",
+        imgSrc: "/location2.png"
+    },
+    {
+        id:2,
+        title: "환불정책",
+        imgSrc: "/location3.png"
+    }
+]
 
 export default function Page() {
     const [content, setContent] = useState<string>("매칭그룹 소개");
-    
-     const buttons = [
-        "매칭그룹 소개",
-        "매칭 프로세스",
-        "환불정책"
-    ];
     
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const buttonText = (event.target as HTMLButtonElement).innerText;
@@ -22,18 +35,18 @@ export default function Page() {
     return (
         <>
             <div className={styles.container}>
-                {buttons.map((text) => (
+                {IMGINFO.map((item) => (
                     <button 
-                        key={text}
+                        key={item.title}
                         onClick={handleClick} 
-                        className={`${styles.btn} ${content === text ? styles.selected : ""}`}
+                        className={`${styles.btn} ${content === item.title ? styles.selected : ""}`}
                     >
-                        {text}
+                        {item.title}
                     </button>
                 ))}
             </div>
             <div className={styles.content}>
-                컨텐츠 자리 
+                <ImgSlider imgInfo={IMGINFO}/>
             </div>
             <NavigationButton url="/" subtitle="결제금액: 2,200원" isValid={true} title="결제 후 매칭룸 입장하기"/>
             <div className={styles.linkWrapper}><Link href="/">매칭 포기하기</Link></div>
