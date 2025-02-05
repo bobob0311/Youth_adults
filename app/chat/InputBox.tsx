57e531bc8cb8d7b51fd49f4670f0eb2d0cf85f67
bosung si <qweasdzxc0311@naver.com>
2025-02-05 20:38:49 +0900

    input box 디자인

--- app/chat/InputBox.tsx
+++ app/chat/InputBox.tsx
@@ -7,31 +7,56 @@ interface PropsState{
     onSend : (message:string) => void 
 }
 
+const MAX_LINE = 8;
+const LINE_HEIGHT = 14;
+
 export default function InputBox(props: PropsState) {
     const { onSend } = props;
-    const inputRef = useRef<HTMLInputElement>(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
     const sendMessage = () => {
-        if (inputRef.current) {
-            onSend(inputRef.current.value);
-            inputRef.current.value = '';
+        if (textareaRef.current) {
+            onSend(textareaRef.current.value);
+            textareaRef.current.value = '';
         }
     }
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
         if (event.key === "Enter") {
+            event.preventDefault();
+            if (textareaRef.current) textareaRef.current.style.height = 'auto';
             sendMessage();
         }
     };
 
+    const handleResizeHeight = () => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = 'auto';
+
+            const maxHeight = MAX_LINE * LINE_HEIGHT;
+
+            if (textareaRef.current.scrollHeight > maxHeight) {
+                textareaRef.current.style.height = `${maxHeight}px`;
+                textareaRef.current.style.overflowY = "auto"; 
+            } else {
+                textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+                textareaRef.current.style.overflowY = "hidden";
+            }
+        }
+    }
+
     
     return (
-        <div>
-            <input
-                ref = {inputRef}
+        <div className={styles.InputContainer}>
+            <div className={styles.box}>
+            <textarea
+                rows={1}
+                ref = {textareaRef}
                 onKeyDown={handleKeyDown}
+                onInput={handleResizeHeight}
             />
-            <button onClick={sendMessage}> 전송 </button>        
+            <button className={styles.sendBtn} onClick={sendMessage}> 전송 </button>        
+            </div>
         </div>
     )
 }
