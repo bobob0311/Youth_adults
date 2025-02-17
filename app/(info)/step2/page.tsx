b68f2b52107ca2711784876a44109ca7f3c024c9
bosung si <qweasdzxc0311@naver.com>
2025-02-17 23:30:57 +0900

    page2 리팩토링

--- app/(info)/step2/page.tsx
+++ app/(info)/step2/page.tsx
@@ -1,44 +1,79 @@
 'use client'
 
-import CheckButton from "@/components/button/CheckButtons";
 import NavigationButton from "@/components/button/NavigationButton";
 import styles from "./selectionPage.module.scss"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeCheck, changeGroupInfo } from "../userSlice";
+import { changeUserInfo } from "../userSlice";
+import ListCheckButton from "../(component)/ListCheck";
+import { RootState } from "@/redux/store";
+
+interface Check{
+    id: string,
+    chk: boolean,
+}
+
+const DUMMY_INFO = [
+    { id: "cnt", label: "인원 수", checkList: ["2명", "3명", "4명", "5명"] },
+    { id: "sex",label: "그룹 성별", checkList: ['여성', '남성', '혼성'] },
+    { id: "old", label: "평균 연령",checkList : ['20대 초','20대 중','20대 말']},
+]
 
 export default function SelectionPage() {
-    const [clicked, setClicked] = useState<string[]>(['', '', '']);
     const [isBtnValid, setIsBtnValid] = useState<boolean>(false);
-    const [validArr, setValidArr] = useState<boolean[]>([false, false, false]);
+    const [validArr, setValidArr] = useState<Check[]>([]);
+   
+    const userInfo = useSelector((state: RootState) => state.user.userInfo);
     const dispatch = useDispatch();
-    const handleStore = () => {
-        dispatch(changeCheck([...clicked]));
-    }
-    
-    const handleChangeState = (item: string, idx: number) => {
-        const newValue = [...clicked];
-        newValue[idx] = item;
-        setClicked(newValue);
-    }
-    const handleValid = (idx:number, isChecked:boolean) => {
-        const updateValidArr = [...validArr];
-        updateValidArr[idx] = isChecked;
-        setValidArr(updateValidArr);
 
-        const allValid = updateValidArr.every((value) => value === true);
-        setIsBtnValid(allValid);
+    const handleStore = (id:string,selected: string) => {
+        dispatch(changeUserInfo({...userInfo, [id]:selected}));
+    }
+    useEffect(() => {
+        const newArr: Check[] = []
+        Object.entries(userInfo).forEach(([key, value]) => {
+            if (value) {
+                newArr.push({ id: key, chk:true});
+            } else {
+                newArr.push({ id: key, chk:false});
+            }
+        })
+        setValidArr(newArr);
+        checkBtnValid(newArr);
+    },[])
+
+    const handleValid = (selectedId:string, isChecked:boolean) => {
+        const updateValidArr = [...validArr];
+        const foundItem = updateValidArr.find(item => item.id === selectedId);
+        if (foundItem) {
+            foundItem.chk = isChecked;
+        }
+        setValidArr(updateValidArr);
+        checkBtnValid(updateValidArr);
+    }
+
+    const checkBtnValid = (nowValidArr: Check[]) => {
+        const allValid = nowValidArr.every((value) => value.chk === true);
+        setIsBtnValid(allValid); 
     }
 
     return (
         <div className={styles.container}>
             <section>
                 <h2 className={styles.title}>그룹 정보를 선택해주세요</h2>
-                <CheckButton onChangeState={(item) =>handleChangeState(item,0) } info={{ type: "label", label: "인원 수", checkList: ['2명', '3명', '4명'] }} onValid={(chk) => handleValid(0,chk) }/>
-                <CheckButton onChangeState={(item) =>handleChangeState(item,1)} info={{ type: "label", label: "그룹 성별", checkList: ['여성', '남성', '혼성'] }} onValid={(chk) => handleValid(1,chk) }/>
-                <CheckButton onChangeState={(item) =>handleChangeState(item,2) } info= {{type: "label", label: "평균 연령",checkList : ['20대 초','20대 중','20대 말']}} onValid={(chk) => handleValid(2,chk) }/>
+                {
+                    DUMMY_INFO.map((item) => (
+                        <ListCheckButton
+                            key={item.label}
+                            listInfo={item}
+                            storedIdx={userInfo[item.id]? item.checkList.indexOf(userInfo[item.id]): -1}
+                            onValid={(selectedId, chk) => handleValid(selectedId, chk)}
+                            onSeletedChange={(id,selected) => handleStore(id,selected)}
+                        />
+                    ))
+                }
             </section>
-            <NavigationButton onStore={handleStore} isValid={isBtnValid} title="다음으로" url="/step3" />
+            <NavigationButton isValid={isBtnValid} title="다음으로" url="/step3" />
         </div>
     )
 }
