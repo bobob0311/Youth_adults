d2cb1e48e42a52b97c536aaefca23e0ab07f5701
bosung si <qweasdzxc0311@naver.com>
2025-02-27 20:54:52 +0900

    matching 페이지 간단 소개글

---
+++ app/match/[id]/Introduce.tsx
@@ -0,0 +1,15 @@
+import styles from "./Introduce.module.scss"
+
+export default function Introduce(props) {
+    const { userInfo } = props;
+    const { group_name, group_description, gender, age } = userInfo;
+    return (
+        <div className={styles.box}>
+            <div>이번에 소개할 팀은 <span className={styles.var}>{group_name}</span>입니다!</div>
+            <div><span className={styles.var}>{age}의 {gender} {age}</span>으로 이루어진,</div>
+            <div>다소 매력적인 팀이랄까요? 살짝만 더 말씀드리면,</div>
+            <div className={styles.var}>{group_description }</div>
+            <div>재밌게 놀 준비 되셨으면 바로 연결해드릴게요!!</div>
+        </div>
+    )
+}
