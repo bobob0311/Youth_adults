8ead9fd62e14cc02feb2906dcb8555bf177b340c
bosung si <qweasdzxc0311@naver.com>
2025-03-01 23:48:17 +0900

    -

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,29 +1,28 @@
 import { createServerClient } from '@supabase/ssr'
-  import { cookies } from 'next/headers'
+import { cookies } from 'next/headers'
 
-  export async function createClient() {
-    const cookieStore = await cookies()
+export async function createClient() {
+  const cookieStore = await cookies()
 
-    return createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          getAll() {
-            return cookieStore.getAll()
-          },
-          setAll(cookiesToSet) {
-            try {
-              cookiesToSet.forEach(({ name, value, options }) =>
-                cookieStore.set(name, value, options)
-              )
-            } catch {
-              // The `setAll` method was called from a Server Component.
-              // This can be ignored if you have middleware refreshing
-              // user sessions.
-            }
-          },
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
         },
-      }
-    )
-  }
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          )
+        },
+      },
+    }
+  )
+
+  // 클라이언트 생성 후, 제대로 supabase가 생성되었는지 확인하는 로그
+  console.log(supabase)
+
+  return supabase
+}
