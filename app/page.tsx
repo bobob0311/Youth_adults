f9eea0d315c00b9e5098730930b040cc5c022c1c
bosung si <qweasdzxc0311@naver.com>
2025-03-14 21:40:43 +0900

    오프닝 페이지 레이아웃 변경

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className={styles.content}>
 
         <h1 className={styles.mainImg}>
-          <Image width={175} height={62} src="/mainImg.png" alt="청춘 상회 이미지" />
+          <Image width={300} height={220} src="/startPage.png" alt="청춘 상회 이미지" />
           <span className={styles.srOnly}>청춘 상회</span>
         </h1>
         <h2 className={styles.subtitle}>내 친구가 되어라. 청춘을 후회없이 즐기세요!</h2>
