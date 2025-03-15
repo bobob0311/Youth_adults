1fad5ad25ce0aa51cb2bbe6e26ba212c9ea93ab1
bosung si <qweasdzxc0311@naver.com>
2025-03-15 19:18:20 +0900

    선택 상자 component로 분리

--- app/match/[id]/Modal.tsx
+++ app/match/[id]/Modal.tsx
@@ -1,40 +1,35 @@
 'use client'
 import { createPortal } from 'react-dom';
 import styles from "./Modal.module.scss"
+import { useRouter } from 'next/navigation';
+import ChooseMathingBox from '../(component)/chooseMatchingBox';
 
 export function Modal({ onModal }: { onModal: () => void }) {
+  const router = useRouter();
+
+  const text = "매칭을 정말 포기하시겠어요?";
+  
   const handleFindAnother = () => {
-    // 다른 매칭 찾기 로직
+    router.push("/match/researching")
   }
 
   const handleGiveUp = () => {
+    router.push("/match/fail");
     // 매칭 포기 로직
   }
 
+  const btnInfo = {
+    firstBtnName: "다른 매칭 찾기",
+    firstBtnFn: handleFindAnother,
+    secondBtnName: "매칭 포기",
+    secondBtnFn: handleGiveUp,
+  }
+
+
+
   return createPortal(
     <div className={styles.modalBackdrop}>
-      <div className={styles.modal}>
-        <button className={styles.closeBtn} onClick={() => onModal()}>
-          <img src='/close.svg'/>
-        </button>
-        
-        <div className={styles.imgContainer}>
-          <img src="/modalImg.png"/>
-        </div>
-        
-        <h3 className={styles.title}>매칭을 정말 포기하시겠어요?</h3>
-        
-        <div className={styles.pBox}>
-          <p>매칭이 마음에 들지 않으면</p>
-          <p>다른 매칭을 찾아 볼 수도 있어요.</p>
-        </div>
-        
-        <div className={styles.btnContainer}>
-          <button onClick={()=> {handleFindAnother()}}>다른 매칭 찾기</button>
-          <button onClick={()=> {handleGiveUp()}}>매칭 포기</button>
-        </div>
-      </div>
-      
+      <ChooseMathingBox onClose={() => onModal()} text={text} btnInfo={btnInfo} />
     </div>,
     document.getElementById('modal-root')!
   );
