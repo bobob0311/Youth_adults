276b6d1d56686931346947c09fc51b37ef4182ac
bosung si <qweasdzxc0311@naver.com>
2025-03-17 19:59:01 +0900

    rematching에 매치아이디, 매치네임 삭제

--- app/match/cancel/page.tsx
+++ app/match/cancel/page.tsx
@@ -1,14 +1,35 @@
 'use client'
+import { useSearchParams } from "next/navigation";
 import ChooseMathingBox from "../(component)/chooseMatchingBox";
+import userApiHandler from "../../../utils/apiHandler/user";
 
 export default function CancelMatchingPage() {
+    const params = useSearchParams();
+    const myId = params.get("id");
+
     const text = "매칭을 정말 그만두시겠어요?"
     
-    const handleRematching = () => {
+    const handleRematching = async () => {
+        if (myId) {
+            const updateInfo = {
+                id: myId,
+                info: [
+                    { column: "matchedId", value: null },
+                    { column: "matchedName", value: null},
+                ]
+            }
+            try{
+                userApiHandler.changeUserData(updateInfo)
+            } catch (e) {
+                console.log("데이터 변경 중 오류가 발생했습니다", e);
+            }
+        } else {
+            throw Error("사용자 ID가 없습니다")
+        }
 
     }
     const handlePaymentCancel = () => {
-
+        //결제 취소 아직 미정
     }
     
     const btnInfo = {
