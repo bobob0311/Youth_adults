61aeeee333576e6b60ba2566d384f1ac9f550d90
bosung si <qweasdzxc0311@naver.com>
2025-03-19 00:48:55 +0900

    phoneValid 오류 픽스, loading로직 추가 중

--- app/(info)/step4/page.tsx
+++ app/(info)/step4/page.tsx
@@ -6,13 +6,14 @@ import styles from "./page.module.scss"
 import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changePhoneNumber } from "../userSlice";
-import { checkNumber, checkNumberLength, checkPhoneNumber } from "@/utils/regex";
+import { checkNumber, checkNumberLength, checkPhone } from "@/utils/regex";
 import { insertUserData, sendAligoMessage } from "@/utils/api";
 import { RootState } from "@/redux/store";
 import { changeUserFormat } from "@/utils/dataFomat";
 import matching from "@/utils/matching";
 import { makeValidCode } from "@/utils/message";
 import Counter from "./counter";
+import { UserState } from "@/types/user";
 
 const PHONEDATA = [
     {
@@ -29,34 +30,48 @@ const PHONEDATA = [
     }
 ]
 
+const PHONE_VALID_CONDITION:Valid = {
+    onInputCondition : (input:string) => {
+        return checkNumber(input) && checkNumberLength(input,11);
+    },
+    onValidCondition : (input: string) => {
+        return checkPhone(input);
+    },
+}
+
+interface Valid {
+    onInputCondition: (text: string) => boolean; 
+    onValidCondition: (text: string) => boolean;
+}
+
 export default function Page() {
     const verificationCodeRef = useRef<number>(undefined);
     const verificationTimeRef = useRef<number>(undefined);
     const myCodeRef = useRef<number>(undefined);
     const phoneNumberRef = useRef<string>('');
+    const phoneNumberValid = useRef<boolean>(false);
     
-    const [isBtnValid, setIsBtnValid] = useState<boolean| null>(null);
-    const [isPhoneNumberValid, setIsPhoneNumberValid] = useState<boolean>(false);
+    const [isBtnValid, setIsBtnValid] = useState<string>("wait");
     const [isSend, setIsSend] = useState<boolean>(false);
-    
+    const [loading, setLoading] = useState<boolean>(false);
+
     const dispatch = useDispatch();
 
-    const userInfo = useSelector((state: RootState) => state.user);
+    const userInfo:UserState = useSelector((state: RootState) => state.user);
     
     const handleInput = (newValue: string, category: string, isValid:boolean) => {
         if (category === "phoneNumber") {
             phoneNumberRef.current = newValue;
-            setIsPhoneNumberValid(isValid);
-
+            phoneNumberValid.current = isValid;
         } else if (category === "authNumber") {
             myCodeRef.current = Number(newValue)
         }
     }
 
     const handleSendMessage = () => {
-        setIsBtnValid(null);
+        setIsBtnValid("wait");
 
-        if (isPhoneNumberValid) {
+        if (phoneNumberValid.current) {
             setIsSend(true);
 
             setTimeout(() => {
@@ -75,7 +90,7 @@ export default function Page() {
         
     }
 
-    const sendVerificationCodeMessage = (phoneNumber,code) => {
+    const sendVerificationCodeMessage = (phoneNumber:string,code:number) => {
         const info = { phoneNumber, code };
         const messageInfo = makeValidCode(info);
         sendAligoMessage(messageInfo);
@@ -85,72 +100,75 @@ export default function Page() {
     const handleCheckNumber = () => {
         if (phoneNumberRef.current) {
             if (myCodeRef.current === verificationCodeRef.current) {
-                setIsBtnValid(true);
+                setIsBtnValid("success");
             } else {
-                setIsBtnValid(false);
+                setIsBtnValid("fail");
                 setTimeout(() => {
-                    setIsBtnValid(null);
+                    setIsBtnValid("wait");
                 },2000)
             }    
         }
     }
 
-    const phoneNumberValidCondition = {
-        onInputCondition : (input:string) => {
-            return checkNumber(input) && checkNumberLength(input,11);
-        },
-        onValidCondition : (input: string) => {
-            return checkPhoneNumber(input);
-        },
+    async function handleClickNavBtn(userInfo: UserState) {
+        setLoading(true);
+        try {
+            const userData = changeUserFormat(userInfo);
+            await insertUserData(userData);
+            matching();
+        } catch (error) {
+            console.error("Error during the process:", error);
+        }
     }
 
-    const userData = changeUserFormat(userInfo)
+    const handleFail = () => {
+        setLoading(false);
 
-    async function insertData(userData) {
-        insertUserData(userData);
-        matching();
-    }    
+    }
 
     return (
-        <div className={styles.container}>
-            <section>
-                <h2 className={styles.title}>문자를 통해 인증을 받아주세요</h2>
-                <div className={styles.wrapper}>
-                    <InputBox
-                        inputInfo={PHONEDATA[0]}
-                        valid={phoneNumberValidCondition}
-                        onText={(newValue, category,isValid) => handleInput(newValue,category,isValid)}
-                    />
-                    <button 
-                        className={`${styles.btn} ${isSend ? styles.send: undefined}`}
-                        onClick={isSend ? undefined : handleSendMessage}
-                    >
-                        {isSend ?
-                            <>
-                                인증번호 문자 발송완료! 재전송은
-                                    < Counter countTime={30} onTime={(time)=> {verificationTimeRef.current = time}}/>
-                                초 후에 할 수 있어요.
-                            </>
-                            : "인증번호 받기"
-                            }
-                    </button>
-                </div>
-                <div className={styles.wrapper}>
-                    <InputBox
-                        inputInfo={PHONEDATA[1]}
-                        valid={undefined}
-                        onText={(newValue, category,isValid) => handleInput(newValue,category,isValid)}
-                    />
-                    <button
-                        className={styles.btn}
-                        onClick={handleCheckNumber}
-                    >
-                        인증번호 확인
-                    </button>
-                </div>
-                {isBtnValid === false? <div className={styles.btnFailMsg}>인증에 실패하였습니다. 잠시 후 다시 시도해주세요!</div>: null}
-            </section>
-            <NavigationButton onStore={() => {insertData(userData)}} isValid={isBtnValid} title="다음으로" url="done" />
-        </div>
+        <>
+            <div className={styles.container}>
+                <section>
+                    <h2 className={styles.title}>문자를 통해 인증을 받아주세요</h2>
+                    <div className={styles.wrapper}>
+                        <InputBox
+                            inputInfo={PHONEDATA[0]}
+                            valid={PHONE_VALID_CONDITION}
+                            onText={(newValue, category,isValid) => handleInput(newValue,category,isValid)}
+                        />
+                        <button 
+                            className={`${styles.btn} ${isSend ? styles.send: undefined}`}
+                            onClick={isSend ? undefined : handleSendMessage}
+                        >
+                            {isSend ?
+                                <>
+                                    인증번호 문자 발송완료! 재전송은
+                                        < Counter countTime={30} onTime={(time)=> {verificationTimeRef.current = time}}/>
+                                    초 후에 할 수 있어요.
+                                </>
+                                : "인증번호 받기"
+                                }
+                        </button>
+                    </div>
+                    <div className={styles.wrapper}>
+                        <InputBox
+                            inputInfo={PHONEDATA[1]}
+                            valid={undefined}
+                            onText={(newValue, category,isValid) => handleInput(newValue,category,isValid)}
+                        />
+                        <button
+                            className={styles.btn}
+                            onClick={handleCheckNumber}
+                        >
+                            인증번호 확인
+                        </button>
+                    </div>
+                    {isBtnValid === "fail"? <div className={styles.btnFailMsg}>인증에 실패하였습니다. 잠시 후 다시 시도해주세요!</div>: null}
+                </section>
+                <NavigationButton onFail={()=> handleFail()} onAction={() => {handleClickNavBtn(userInfo)}} isValid={isBtnValid === "success"} title="다음으로" url="done" />
+            </div>
+            {loading && <div>로딩컴포넌트 모달 들어갈듯</div>}
+        </>
     )
 }
