b86d469af8e192150ccd29af79e19936b92062d5
bosung si <qweasdzxc0311@naver.com>
2025-03-24 23:23:55 +0900

    matching 취소

---
+++ app/match/cancel/match/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { useSearchParams } from "next/navigation";
+import ChooseMathingBox from "../../_component/chooseMatchingBox";
+import { Suspense } from "react";
+import LoadingSpinner from "@/components/loading/LoadingSpinner";
+
+
+function CancelMatching() {
+    const params = useSearchParams();
+    const myId = params.get("id");
+
+    const text = "매칭을 정말 그만두시겠어요?"
+    
+    const handleStopMatching = () => {
+        // id를 기준으로 matching에 들어가지 않게 db에서 처리해야할듯.
+    }
+    const handleClosePage = () => {
+        window.close();
+    }
+    
+    const btnInfo = {
+        firstBtnName: "매칭 중단",
+        firstBtnFn: handleStopMatching,
+        secondBtnName: "창 닫기",
+        secondBtnFn: handleClosePage
+    }
+    
+    
+
+    
+    return (
+        <ChooseMathingBox text={text} btnInfo={btnInfo} />
+    )
+}
+
+export default function CancelMatchingPage() {
+    return (
+        <Suspense fallback={<LoadingSpinner/>}>
+            <CancelMatching/>
+        </Suspense>
+    )
+}
