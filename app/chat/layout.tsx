b68fcc7754d8cfee96222aa1b077ac483050435c
bosung si <qweasdzxc0311@naver.com>
2025-03-27 20:48:06 +0900

    chat 리매칭 버튼 UI생성

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Image from "next/image";
 import styles from "./layout.module.scss";
 
@@ -8,6 +6,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <header className={styles.header}>
+                <span>
+                    <button className={styles.rematchBtn}>
+                        <Image width={25} height={25} src="/rematchBtn.svg" alt="매칭 다시 찾기"/>
+                        <div>매칭 찾기</div>
+                    </button>
+                </span>
                 <h1 className={styles.title}><Image width={175} height={65} src="/mainImg.png" alt="청춘 상회"/></h1>
             </header>
             <main>
