8306ea83bfeeb8cd635dc42a32c4c1fe27f772bb
bosung si <qweasdzxc0311@naver.com>
2025-04-02 23:57:17 +0900

    이미지 이미지

--- app/match/done/page.tsx
+++ app/match/done/page.tsx
@@ -15,6 +15,7 @@ export default function DonePage() {
                 alt="종료 이미지"
                 src="/end.png"
                 unoptimized={true}
+                priority={true}
             />
             <p>메시지를 통해 링크를 전송해 드릴게요!</p>
             <span>매칭 링크 전송은 최대 10분까지 소요될 수 있습니다.</span>
