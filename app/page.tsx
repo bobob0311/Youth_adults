e844eb3f7f68c84e355de2a1111246b5d7f861f6
bosung si <qweasdzxc0311@naver.com>
2025-04-04 02:06:35 +0900

    시작 페이지 서버에서 내리게 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,11 @@
-'use client'
-import { useEffect, useState } from "react";
 import OpenPage from "@/components/start/Open";
 import ClosePage from "@/components/start/Close";
 
 export default function Home() {
-  const [isAvailable, setIsAvailable] = useState(false);
- 
-  useEffect(() => {
-    const today = new Date();
-    const dayOfWeek = today.getDay();
-    if (dayOfWeek === 5 || dayOfWeek === 6 || dayOfWeek === 0) {
-      setIsAvailable(true);
-    } else {
-      setIsAvailable(false);
-    }
-  }, [])
-  
-  
-  return (
-    <>
-      {isAvailable? <OpenPage/>: <ClosePage/>}
-    </>
-  )
+  const now = new Date();
+  const koreaTime = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+  const dayOfWeek = koreaTime.getUTCDay();
+  const isAvailable = dayOfWeek === 5 || dayOfWeek === 6 || dayOfWeek === 0;
+
+  return isAvailable ? <OpenPage /> : <ClosePage />;
 }
