e0155bc7ce30827dc134120a3bcc8b0c72d9439a
bosung si <qweasdzxc0311@naver.com>
2025-04-04 03:18:39 +0900

    done page 정적

--- app/(info)/done/page.tsx
+++ app/(info)/done/page.tsx
@@ -1,12 +1,20 @@
-'use client'
 import Image from 'next/image';
 import styles from "./page.module.scss";
+
+export const dynamic = 'force-static'
 
 export default function DonePage() {
 
     return (
         <div className={styles.wrapper}>
-            <Image  width={320} height={260} className={styles.backGound} alt="종료 이미지" src="/end.png" />
+            <Image
+                width={320}
+                height={260}
+                className={styles.backGound}
+                alt="종료 이미지"
+                src="/end.png"
+                priority
+            />
             <p>신청이 완료되었어요!</p>
             <p>매칭이 되면 연락드릴게요</p>
         </div>
