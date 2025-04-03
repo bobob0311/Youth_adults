6b0ba05139f4a6fd9c61bb28b7bfab65485aee90
bosung si <qweasdzxc0311@naver.com>
2025-04-04 03:20:10 +0900

    age 다시 돌려서 렌더링하기

--- app/match/_component/_MatchAction/Introduce.tsx
+++ app/match/_component/_MatchAction/Introduce.tsx
@@ -1,12 +1,14 @@
+import { reverseChangeOld } from "@/utils/dataFomat";
 import styles from "./Introduce.module.scss"
 
 export default function Introduce(props) {
     const { userInfo } = props;
     const { group_name, group_description, gender, age, member_count } = userInfo;
+    const calcAge = reverseChangeOld(age)
     return (
         <div className={styles.box}>
             <div>이번에 소개할 팀은 <span className={styles.var}>{group_name}</span>입니다!</div>
-            <div><span className={styles.var}>{age}의 {gender} {member_count}</span>으로 이루어진,</div>
+            <div><span className={styles.var}>{calcAge}의 {gender} {member_count}</span>으로 이루어진,</div>
             <div>다소 매력적인 팀이랄까요?</div><div> 살짝만 더 말씀드리면,</div>
             <div className={styles.var}>{group_description }</div>
             <div>재밌게 놀 준비 되셨으면</div>
