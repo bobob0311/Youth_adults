230eabfb6324213bd556a1f02f9bb627ed85b7da
bosung si <qweasdzxc0311@naver.com>
2025-04-04 01:53:24 +0900

    방 나간경우 UI 및 로직

--- app/chat/ChatRoom.tsx
+++ app/chat/ChatRoom.tsx
@@ -8,18 +8,21 @@ import MessageContainer from "./_component/MessageContainer";
 import InputBox from "./_component/InputBox";
 import styles from "./ChatRoom.module.scss";
 import WrapperLayout from "./WrapperLayout";
+import RematchContainer from "./_component/_inputBox/RematchContainer";
+import { leaveRoom } from "@/apiHandler/room";
 
-export default function ChatRoom({userId, roomId}: {userId:string, roomId: string}) {
+export default function ChatRoom({userId, roomId,roomStatus}: {userId:string, roomId: string, roomStatus:boolean}) {
     const [rematch, setRematch] = useState(false);
+    const [isRoomOpen, setIsRoomOpen] = useState(roomStatus);
     const userData = useUser(userId || "");
-
+    
     const roomInfo = userData ?
         {
             myGroupName: userData.group_name,
-            otherGroupName: userData.matched_name,
+            otherGroupName: userData.matchedName,
             myGroupId: userData.id,
-            otherGroupId: userData.matched_id,
-            isFirst: userData.first_in
+            otherGroupId: userData.matchedId,
+            isFirst: userData.firstIn
         }
     :
         null;
@@ -35,12 +38,27 @@ export default function ChatRoom({userId, roomId}: {userId:string, roomId: strin
         <WrapperLayout onRematchClick={handleCheckRematch}>
             <div className={styles.chatContainer}>
                 <MessageContainer messages={messages} roomInfo={roomInfoRef.current} />
-                <InputBox
-                    onRematch={() => setRematch(prev => !prev)}
-                    rematch={rematch}
-                    onSend={(message) => sendTextMessage(message)}
-                    onImgSend={(imgFile) => sendImgMessage(imgFile)}
-                />
+                {
+                    isRoomOpen ?
+                        <InputBox
+                            onRematch={() => setRematch(prev => !prev)}
+                            rematch={rematch}
+                            onSend={(message) => sendTextMessage(message)}
+                            onImgSend={(imgFile) => sendImgMessage(imgFile)}
+                            onLeaveRoom={() => leaveRoom(roomId,userId)}
+                        />
+                        :
+                        <div className={styles.outContainer}>
+                            <RematchContainer
+                                title="상대가 매칭룸을 나갔어요."
+                                subTitle="다른 매칭 찾기를 선택하시면 우선적으로 매칭을 찾아드려요"
+                                leftBtn={{name: "매칭 취소" , fn :() => { console.log("아직 안했습니다.") }}}
+                                rightBtn={{name: "다른 매칭 찾기" , fn :() => { leaveRoom(roomId,userId) }}}
+                            />
+                        </div>
+                }
+                
+                
             </div>
         </WrapperLayout> 
     );
