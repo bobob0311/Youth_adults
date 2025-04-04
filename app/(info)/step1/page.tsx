ea4a163e16bbe1f073fea50c067133df6174d9c3
bosung si <qweasdzxc0311@naver.com>
2025-04-04 21:02:16 +0900

    step1 리팩토링

--- app/(info)/step1/page.tsx
+++ app/(info)/step1/page.tsx
@@ -4,38 +4,25 @@ import styles from "./LocationPage.module.scss";
 import NavigationButton from "@/components/button/NavigationButton";
 import LocationCheckButton from "../(component)/LocationCheck";
 
-import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { changeLocation } from "../userSlice";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { LOCATION_INFO } from "@/utils/dummyData";
+import { useSelectionState } from "@/hooks/useSelectionState";
 
-const DUMMY_INFO = [
-    { locationName: "강남/신논현", imgUrl: "/gangnam.png" },
-    { locationName: "홍대/합정", imgUrl: "/hongdae.png" },
-    { locationName: "이태원", imgUrl: "/itaewon.png" },
-    { locationName: "건대입구", imgUrl: "/konkuk.png" },
-]
 
 export default function LocationPage() {
-    const [isBtnValid, setIsBtnValid] = useState<boolean>(false);
-    
-    const dispatch = useDispatch();
-    const location = useSelector((state:RootState) => state.user.location);
-    
+    const { selectedValue: location, dispatch, isValid: isBtnValid, setIsValid: setIsBtnValid } =
+        useSelectionState<string>((state: RootState) => state.user.location);
+
     useEffect(() => {
         if (location !== '') {
             setIsBtnValid(true);
-        } else {
-            setIsBtnValid(false);
         }
     }, [location])
 
     const handleLocationChange = (location: string) => {
-        if (location !== '') {
-            dispatch(changeLocation(location));
-        } else {
-            dispatch(changeLocation(''));
-        }
+        dispatch(changeLocation(location))
     }
 
     return (
@@ -43,7 +30,7 @@ export default function LocationPage() {
             <section className={styles.location}>
                 <h2 className={styles.title}>현재 위치를 선택해주세요</h2>
                 <LocationCheckButton
-                    locationInfo={DUMMY_INFO}
+                    locationInfo={LOCATION_INFO}
                     storedLocation={location}
                     onLocationChange={handleLocationChange}
                 />
