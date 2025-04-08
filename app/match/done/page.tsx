59cef3896c0535bcef18923d15a29f85557432a1
bosung si <qweasdzxc0311@naver.com>
2025-04-08 22:30:36 +0900

    done Page 애니메이션

--- app/match/done/page.tsx
+++ app/match/done/page.tsx
@@ -1,21 +1,19 @@
 'use client'
 import Image from "next/image";
+import Lottie from "lottie-react"; 
+import animationData from "@/assets/done.json"; 
 import styles from "./page.module.scss";
 
 export const dynamic = "force-static";
 
 export default function DonePage() {
-
     return (
         <div className={styles.wrapper}>
-            <Image
-                width={320}
-                height={260}
-                className={styles.backGound}
-                alt="종료 이미지"
-                src="/end.png"
-                unoptimized={true}
-                priority={true}
+            <Lottie
+                animationData={animationData}
+                loop
+                autoplay
+                style={{ width: 600, height: 500 }} 
             />
             <p>메시지를 통해 링크를 전송해 드릴게요!</p>
             <span>매칭 링크 전송은 최대 10분까지 소요될 수 있습니다.</span>
