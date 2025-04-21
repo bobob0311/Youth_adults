ce097e0ec200da53c7862d70de5049e5d437b40b
bosung si <qweasdzxc0311@naver.com>
2025-04-21 20:09:18 +0900

    시간

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { toZonedTime } from "date-fns-tz";
 import OpenPage from "@/components/start/Open";
 import ClosePage from "@/components/start/Close";
 
 export default function Home() {
   const now = new Date();
-  const koreaTime = new Date(now.getTime() + 9 * 60 * 60 * 1000);
-  const dayOfWeek = koreaTime.getUTCDay();
+  const timeZone = "Asia/Seoul";
+  const koreaTime = toZonedTime(now, timeZone);
+
+  const dayOfWeek = koreaTime.getDay(); // 0 (일) ~ 6 (토)
   const isAvailable = dayOfWeek === 5 || dayOfWeek === 6 || dayOfWeek === 0;
 
   return isAvailable ? <OpenPage /> : <ClosePage />;
