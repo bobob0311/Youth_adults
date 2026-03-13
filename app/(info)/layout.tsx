6098052739bc92c1bf457d7f88c33177e2e2c44e
bosung <qweasdzxc0311@naver.com>
2026-03-14 00:42:23 +0900

    refactor: 파일 구조 정리로 인한 변경

--- app/(info)/layout.tsx
+++ app/(info)/layout.tsx
@@ -1,48 +1,40 @@
-'use client'
+"use client";
 
 import { usePathname, useRouter } from "next/navigation";
 import styles from "./layout.module.scss";
-import ProgressBar from "@/components/ProgressBar";
+import ProgressBar from "@/features/signup/components/ProgressBar";
 import Image from "next/image";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const router = useRouter();
-    const path = usePathname();
-    
-    const isStep = (path:string) => {
-        if (path.substring(1, 5) === "step") {
-            
-            return  <ProgressBar range={path[5]}/>;
-        } else {
-            return null;
-        }
-    }
+  const router = useRouter();
+  const path = usePathname();
 
-    return (
-        <>
-            <header className={styles.header}>
-                <button
-                    className={styles.btn}
-                    onClick={() => router.back()}
-                    aria-label="뒤로 가기 버튼"
-                >
-                    
-                    <Image width={25} height={25}  src="/Back.svg" alt="뒤로가기"/>
-                </button>
-                <h1 className={styles.title}>
-                    <Image
-                        width={120}
-                        height={40}
-                        src="/mainImg.png"
-                        alt="청춘 상회"
-                    /></h1>
-            </header>
-            <main>
-                {isStep(path)}
-                <div className={styles.content}>
-                    {children}
-                </div>
-            </main>
-        </>
-    )
+  const isStep = (path: string) => {
+    if (path.substring(1, 5) === "step") {
+      return <ProgressBar range={path[5]} />;
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <>
+      <header className={styles.header}>
+        <button
+          className={styles.btn}
+          onClick={() => router.back()}
+          aria-label="뒤로 가기 버튼"
+        >
+          <Image width={25} height={25} src="/Back.svg" alt="뒤로가기" />
+        </button>
+        <h1 className={styles.title}>
+          <Image width={120} height={40} src="/mainImg.png" alt="청춘 상회" />
+        </h1>
+      </header>
+      <main>
+        {isStep(path)}
+        <div className={styles.content}>{children}</div>
+      </main>
+    </>
+  );
 }
