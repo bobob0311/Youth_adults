e2aa6851d846e93d5483aa54dd7ccf7ee93d6c77
bosung <qweasdzxc0311@naver.com>
2026-04-06 23:27:32 +0900

    refator: userDetail RHF와 zod를 활용한 로직으로 리팩토링

--- app/(info)/step3/page.tsx
+++ app/(info)/step3/page.tsx
@@ -1,57 +1,63 @@
 "use client";
 
-import { useState } from "react";
-import NavigationButton from "@/shared/components/NavigationButton";
 import styles from "./page.module.scss";
 import { changeUserDetail } from "@/features/signup/model/userSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { GROUP_VALID_CONDITION } from "@/features/signup/constants";
-import { UserDetails } from "@/features/signup/types/user";
 import InputBox from "@/features/signup/components/InputBox";
 import { GROUP_INPUT_INFO } from "@/features/signup/constants";
-import { isUserDetailsValid } from "@/features/signup/helper";
+
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import {
+  type UserDetailsFormValues,
+  type UserDetailsSubmitValues,
+  userDetailsSchema,
+} from "@/features/signup/schema/userDetails";
+import { zodResolver } from "@hookform/resolvers/zod";
+import SubmitButton from "@/shared/components/Button/SubmitButton";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const dispatch = useAppDispatch();
   const storedUserDetails = useAppSelector((state) => state.user.userDetails);
+  const router = useRouter();
 
-  const [formValues, setFormValues] = useState<UserDetails>(storedUserDetails);
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<UserDetailsFormValues, any, UserDetailsSubmitValues>({
+    resolver: zodResolver(userDetailsSchema),
+    defaultValues: storedUserDetails,
+    mode: "onChange",
+  });
 
-  const handleInputChange = (newValue: string, category: keyof UserDetails) => {
-    setFormValues((prev) => ({
-      ...prev,
-      [category]: newValue,
-    }));
+  const onSubmit: SubmitHandler<UserDetailsSubmitValues> = (values) => {
+    dispatch(changeUserDetail(values));
+    router.push("/step4");
   };
-
-  const handleNext = () => {
-    dispatch(changeUserDetail(formValues));
-  };
-
-  const isBtnValid = isUserDetailsValid(formValues);
 
   return (
-    <div className={styles.container}>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
       <section>
         <h2 className={styles.title}>그룹을 간단하게 소개해주세요</h2>
 
         {GROUP_INPUT_INFO.map((item) => (
-          <InputBox
+          <Controller
             key={item.category}
-            inputInfo={item}
-            valid={GROUP_VALID_CONDITION[item.category]}
-            value={formValues[item.category] ?? ""}
-            onText={(newValue) => handleInputChange(newValue, item.category)}
+            control={control}
+            name={item.category as keyof UserDetailsFormValues}
+            render={({ field }) => (
+              <InputBox
+                inputInfo={item}
+                value={field.value ?? ""}
+                onText={field.onChange}
+              />
+            )}
           />
         ))}
       </section>
 
-      <NavigationButton
-        onAction={handleNext}
-        isValid={isBtnValid}
-        title="다음으로"
-        url="/step4"
-      />
-    </div>
+      <SubmitButton title="다음으로" isValid={isValid} />
+    </form>
   );
 }
